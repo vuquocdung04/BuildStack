b034d0ef383a4532fbb472ddc41aad4599b1b5f9
Dung <s5ipart5s@gmail.com>
2025-02-03 10:36:22 +0700

    update script singleton

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,10 +1,10 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Main : MonoBehaviour
+public class Main : Singleton<Main>
 {
     [SerializeField] GameObject CurrentCube;
     [SerializeField] GameObject LastCube;
@@ -12,17 +12,29 @@ public class Main : MonoBehaviour
     [SerializeField] int level;
     [SerializeField] bool done;
     [SerializeField] float cubeSpeed;
+    [SerializeField] float thresHold;
+
     private void Start()
     {
         NewBlock();
     }
     void Update()
     {
-        if(done) return;
+        if (done)
+        {
+            Camera mainCamera = Camera.main;
+
+            float targetFOV = 90f;
+            float currentFOV = mainCamera.fieldOfView;
+            mainCamera.fieldOfView = Mathf.Lerp(currentFOV, targetFOV, Time.deltaTime * 5f);
+            return;
+        }
 
         var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
+
         var pos1 = LastCube.transform.position + Vector3.up * 10f;
-        var pos2 = pos1 + ((level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+
+        var pos2 = pos1 + ((level % 2 == 0) ? Vector3.left : -Vector3.forward) * 100;
 
         if (level % 2 == 0)
             CurrentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
@@ -34,22 +46,36 @@ public class Main : MonoBehaviour
 
     protected virtual void NewBlock()
     {
-        if(LastCube != null)
+        if (LastCube != null)
         {
-            CurrentCube.transform.position = new Vector3(CurrentCube.transform.position.x,
-                CurrentCube.transform.position.y,
-                CurrentCube.transform.position.z);
+            float offsetX = Mathf.Abs(CurrentCube.transform.position.x - LastCube.transform.position.x);
+            float offsetZ = Mathf.Abs(CurrentCube.transform.position.z - LastCube.transform.position.z);
+            if (offsetX <= thresHold)
+            {
+                CurrentCube.transform.position = new Vector3(LastCube.transform.position.x,
+                    CurrentCube.transform.position.y,
+                    CurrentCube.transform.position.z);
 
-            CurrentCube.transform.localScale = new Vector3(LastCube.transform.localScale.x - Mathf.Abs(CurrentCube.transform.position.x - LastCube.transform.position.x),
+                offsetX = 0;
+            }
+
+            if (offsetZ <= thresHold)
+            {
+                CurrentCube.transform.position = new Vector3(CurrentCube.transform.position.x,
+                    CurrentCube.transform.position.y,
+                    LastCube.transform.position.z);
+
+                offsetZ = 0;
+            }
+
+            CurrentCube.transform.localScale = new Vector3(
+                LastCube.transform.localScale.x - offsetX,
                 LastCube.transform.localScale.y,
-                LastCube.transform.localScale.z - Mathf.Abs(CurrentCube.transform.position.z - LastCube.transform.position.z));
+                LastCube.transform.localScale.z - offsetZ);
 
-            CurrentCube.transform.position = Vector3.Lerp(CurrentCube.transform.position,LastCube.transform.position, 0.5f) + Vector3.up * 5f;
+            CurrentCube.transform.position = Vector3.Lerp(CurrentCube.transform.position, LastCube.transform.position, 0.5f) + Vector3.up * 5f;
 
-           
-
-            if(CurrentCube.transform.localScale.x <= 0f || 
-                CurrentCube.transform.localScale.z <= 0f)
+            if (CurrentCube.transform.localScale.x <= 0f || CurrentCube.transform.localScale.z <= 0f)
             {
                 done = true;
                 text.gameObject.SetActive(true);
@@ -61,8 +87,9 @@ public class Main : MonoBehaviour
         CurrentCube = Instantiate(LastCube);
         CurrentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((level / 100f) % 1f, 1f, 1f));
         level++;
-        Camera.main.transform.position = CurrentCube.transform.position + new Vector3(100,100,100);
+        Camera.main.transform.position = CurrentCube.transform.position + new Vector3(150, 150, 150);
         Camera.main.transform.LookAt(CurrentCube.transform.position);
     }
+
 
 }
---
+++ Assets/Scripts/LoadAuto.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAuto : MonoBehaviour
+{
+    protected virtual void Reset()
+    {
+        this.LoadComponents();
+        this.ResetValue();
+    }
+    protected virtual void LoadComponents()
+    {
+
+    }
+    protected virtual void ResetValue()
+    {
+
+    }
+}
